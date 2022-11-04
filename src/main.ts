7ce1fd531eed62e9135f42d3ec8aefd0dcb5173e
Rafael Borges <rafael.borges@luizalabs.com>
2022-11-04 09:55:31 -0300

    configuration

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(3333);
 }
