cd3cad6a7e1de5bed99e6e854d131ff7c5f63034
Rafael Borges <rafael.borges@luizalabs.com>
2022-11-04 09:54:33 -0300

    configure to docker network

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -7,7 +7,8 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          url: 'postgresql://postgres:132@localhost:5432/nest?schema=public',
+          // url: 'postgresql://postgres:132@localhost:5432/nest?schema=public',
+          url: 'postgresql://postgres:132@172.17.0.1:5432/nest?schema=public',
         },
       },
     });
