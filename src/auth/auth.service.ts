8480b66eddc990dd8428b56c977d96a8596a1813
Rafael Borges <rafael.borges@luizalabs.com>
2022-11-04 11:41:54 -0300

    create login services

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,36 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon2 from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
-import * as argon2 from 'argon2';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly prisma: PrismaService) {}
 
   async signup(dto: AuthDto) {
-    console.log('signinProps', dto);
+    try {
+      const hash = await argon2.hash(dto.password);
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash: hash,
+        },
+        select: {
+          id: true,
+          firstName: true,
+          lastName: true,
+          email: true,
+          createdAt: true,
+        },
+      });
 
-    const hash = await argon2.hash(dto.password);
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash: hash,
-      },
-      select: {
-        id: true,
-        firstName: true,
-        lastName: true,
-        email: true,
-        createdAt: true,
-      },
-    });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
 
-    return user;
+      throw error;
+    }
   }
 
-  signin(dto: AuthDto) {
-    return {
-      msg: 'Sign IN!!!',
-    };
+  async signin(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.findFirstOrThrow({
+        where: { email: dto.email },
+      });
+
+      const pwdMatch = await argon2.verify(user.hash, dto.password);
+      if (!pwdMatch) {
+        throw new ForbiddenException();
+      }
+
+      delete user.hash;
+      return user;
+    } catch (error) {
+      throw new ForbiddenException('Credentials incorrect');
+    }
   }
 }
